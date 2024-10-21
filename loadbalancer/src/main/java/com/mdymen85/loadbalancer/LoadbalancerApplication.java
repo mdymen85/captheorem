dc26f5f032db1ced814416c2d47388492a5a5088
Martin <martin.dymenstein@matera.com>
2024-10-20 23:30:19 -0300

    init

---
+++ loadbalancer/src/main/java/com/mdymen85/loadbalancer/LoadbalancerApplication.java
@@ -0,0 +1,13 @@
+package com.mdymen85.loadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadbalancerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadbalancerApplication.class, args);
+	}
+
+}
